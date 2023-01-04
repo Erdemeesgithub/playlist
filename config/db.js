5abb0ab2061d3a060fb1e18f72ac55b9cf0c680b
Erdem <110883062+UrCheryl@users.noreply.github.com>
2023-01-04 22:35:36 +0800

    ld

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Dirty_ToiletPaper:94031299ub@cluster0.hvxh5nv.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Dirty_ToiletPaper:94031299Ub@cluster0.hvxh5nv.mongodb.net/?retryWrites=true&w=majority";
 const connect = async () => {
   try {
     mongoose.set("strictQuery", false);
--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { createSong, getSong, deleteSong } = require("../controller/songController");
+const { createSong, getSong, deleteSong, getSongs } = require("../controller/songController");
 
 const router = express.Router();
 
 router
    .get("/songs", getSongs)
-   .get("/song/:id"), getSong
+   .get("/song/:id", getSong)
    .post("/songs", createSong)
    .put("/song/:id", () => {})
    .delete("/song/:id", deleteSong);
