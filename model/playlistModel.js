00c2bffc1c9d5da09e533715ede2613cdee48335
22PHOP2D-Erdemee <erdemee@LEAP-04s-iMac.local>
2023-01-04 16:34:04 +0800

    g

--- model/playlistModel.js
+++ model/playlistModel.js
@@ -1,11 +1,31 @@
 const { Schema, Types, model } = require("mongoose");
 
 const playlistSchema = new Schema({
-  title: String,
-  description: String,
-  creatorId: Types.ObjectId,
-  createdAt: { type: Date, default: Date.now() },
-  isPrivate: Boolean,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false,
+  },
+  songs: {
+    type: Schema.Types.ObjectId,
+    ref: "Song",
+  },
 });
 
 const Playlist = model("Playlist", playlistSchema);
